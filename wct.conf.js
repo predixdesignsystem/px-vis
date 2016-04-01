b5551ffb8ff8c1d472372b9a8de07e6c49ee195e
Elliot Nahman <elliot.nahman@ge.com>
2016-04-01 09:40:36 -0700

    First commit: basic timeseries draw with navigator, multiple series, tooltip, and register

---
+++ wct.conf.js
@@ -0,0 +1,14 @@
+module.exports = {
+  verbose: true,
+  plugins: {
+    local: {
+      browsers: ['chrome', 'firefox']
+    },
+    sauce: {
+      disabled: true
+    }
+  },
+  suites: [
+    'test/px-vis-test-fixture.html'
+  ]
+};
