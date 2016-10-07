25c7efe9610748f5632db02a9b41a7f0791134bf
212417951 <katherine.menkhaus@ge.com>
2016-10-07 11:55:36 -0700

    ready to go but some errors on load

--- gulpfile.js
+++ gulpfile.js
@@ -81,8 +81,7 @@ gulp.task('serve', function() {
     server: ['./', 'bower_components'],
   });
 
-  gulp.watch(['css/*-styles.html', 'css/*-demo.css', '*.html', '*.js'])
-  // .on('change', browserSync.reload);
+  gulp.watch(['css/*-styles.html', 'css/*-demo.css', '*.html', '*.js']).on('change', browserSync.reload);
   gulp.watch(['sass/*.scss', '!sass/*-demo.scss'], ['sass']);
   gulp.watch('sass/*-demo.scss', ['demosass']);
 
