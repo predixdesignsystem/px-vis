c67a4820d322bd972a600f9a64efe3d7151c811d
212417951 <katherine.menkhaus@ge.com>
2016-10-07 10:56:26 -0700

    cleaned up discriptions and took out animation for this version

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,8 @@ gulp.task('serve', function() {
     server: ['./', 'bower_components'],
   });
 
-  gulp.watch(['css/*-styles.html', 'css/*-demo.css', '*.html', '*.js']).on('change', browserSync.reload);
+  gulp.watch(['css/*-styles.html', 'css/*-demo.css', '*.html', '*.js'])
+  // .on('change', browserSync.reload);
   gulp.watch(['sass/*.scss', '!sass/*-demo.scss'], ['sass']);
   gulp.watch('sass/*-demo.scss', ['demosass']);
 
