d8c5726b59a8680b0e0743f7a3023ca78cda5249
Benoit Chevalier <benoit.chevalier@ge.com>
2017-03-10 12:10:38 -0800

    remove console.log

--- test/px-vis-multi-axis-tests.js
+++ test/px-vis-multi-axis-tests.js
@@ -291,7 +291,6 @@ function runTests(){
       multiAxis.set('chartData',d);
 
       setTimeout(function(){
-        console.log(multiAxis.domainChanged);
         done()}, 1000);
      // done();
     });
