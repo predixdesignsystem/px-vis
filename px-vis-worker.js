d2c82bf1d87146258a3dc8d77fe9fa92a46c59b8
Benoit Chevalier <benoit.chevalier@ge.com>
2017-04-13 13:10:10 -0700

    web worker and chart mapping

--- px-vis-worker.js
+++ px-vis-worker.js
@@ -1,23 +1,35 @@
 
 
+var dataMapping = {},
+    bucketedDataMapping = {};
 
+function reply(data, time) {
 
-
-
-function reply(webWorkerIndex, data) {
-
+  var time2 = this.performance.now();
   if(data) {
-    postMessage({'webWorkerIndex': webWorkerIndex, 'data': data});
+    postMessage({'data': data, 'timeIn': time, 'timeOut': time2});
   } else {
-    postMessage({'webWorkerIndex': webWorkerIndex});
+    postMessage({'timeIn': time, 'timeOut': time2});
   }
+}
+
+function updateData(eventData, time) {
+
+  dataMapping[eventData.chartId] = eventData.data.chartData;
+  reply(null, time);
 }
 
 onmessage = function(e) {
 
+  var time = this.performance.now();
   switch(e.data.action) {
     case 'dummy':
-      reply(e.data.webWorkerIndex);
+      reply(e.data, time);
       break;
+    case 'updateData':
+      updateData(e.data, time);
+      break;
+    default:
+      reply(null, time);
   }
 }
