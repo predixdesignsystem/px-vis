20f3b3fdb12c9fbefc5ddfd1ce66f0eecf971d2d
Benoit Chevalier <benoit.chevalier@ge.com>
2017-07-21 16:11:50 -0700

    move iron-meta import. fix events tests

--- test/px-vis-event-tests.js
+++ test/px-vis-event-tests.js
@@ -2,6 +2,25 @@ document.addEventListener("WebComponentsReady", function() {
   runTests();
 });
 
+function componentFromStr(numStr, percent) {
+    var num = Math.max(0, parseInt(numStr, 10));
+    return percent ?
+        Math.floor(255 * Math.min(100, num) / 100) : Math.min(255, num);
+}
+
+function rgbToHex(rgb) {
+    var rgbRegex = /^rgb\(\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*\)$/;
+    var result, r, g, b, hex = "";
+    if ( (result = rgbRegex.exec(rgb)) ) {
+        r = componentFromStr(result[1], result[2]);
+        g = componentFromStr(result[3], result[4]);
+        b = componentFromStr(result[5], result[6]);
+
+        hex = "#" + (0x1000000 + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+    return hex;
+}
+
 function runTests(){
   suite('px-vis-scatter does Polymer exist?', function() {
     test('Polymer exists', function() {
@@ -128,8 +147,8 @@ function runTests(){
         eventConfig =  {
           "font awesome":{
             "color": "blue",
-            "icon": "fa-camera",
-            "type": "fa",
+            "icon": "px-vis:mov",
+            "type": "px",
             "offset":[0,0]
           },
           "unicode":{
@@ -145,9 +164,9 @@ function runTests(){
             "size":"20"
           },
           "offset":{
-            "icon": "fa-asterisk",
+            "icon": "px-vis:mov",
             "color":"red",
-            "type": "fa",
+            "type": "px",
             "offset":[10,20]
           }
         };
@@ -269,33 +288,17 @@ function runTests(){
     });
 
     test('defaultEvent eventIcon created', function() {
-      assert.equal(defaultEvent.eventIcon.node().tagName,'text');
+      assert.equal(defaultEvent.eventIcon.node().tagName,'g');
     });
-    test('defaultEvent eventIcon font-family', function() {
-      assert.equal(defaultEvent.eventIcon.attr('font-family'),'FontAwesome');
-    });
-    test('defaultEvent eventIcon font-size', function() {
-      assert.equal(defaultEvent.eventIcon.attr('font-size'),'16px');
-    });
-    test('defaultEvent eventIcon fill color', function() {
-      assert.equal(defaultEvent.eventIcon.attr('fill').split(' ').join(''),colors['grey7']);
-    });
-    test('defaultEvent eventIcon icon', function() {
-      // This is weird. the  is the icon, but doesnt render in the text editor since it doesnt have the font file. so pulling the unicode from http://fontawesome.io/cheatsheet/ to test.
-      assert.equal(defaultEvent.eventIcon.text(),'\uf05a');
-    });
-    test('defaultEvent eventIcon x', function() {
-      assert.equal(defaultEvent.eventIcon.attr('x'),232);
-    });
-    test('defaultEvent eventIcon y', function() {
-      assert.equal(defaultEvent.eventIcon.attr('y'),-5);
+    test('defaultEvent eventIcon transform', function() {
+      assert.equal(defaultEvent.eventIcon.attr('transform'), 'translate(232,-21) scale(1)');
     });
 
     test('defaultEvent eventLine created', function() {
       assert.equal(defaultEvent.eventLine.node().tagName,'line');
     });
     test('defaultEvent eventLine stroke color', function() {
-      assert.equal(defaultEvent.eventLine.attr('stroke').split(' ').join(''),colors['grey9']);
+      assert.equal(defaultEvent.eventLine.attr('stroke').split(' ').join(''),rgbToHex(colors['grey8']));
     });
     test('defaultEvent eventLine stroke width', function() {
       assert.equal(defaultEvent.eventLine.attr('stroke-width'),1);
@@ -318,140 +321,6 @@ function runTests(){
     });
     test('tooltip content is correct', function() {
       assert.equal(defaultEvent.$$('px-tooltip').$.tooltip.querySelector('span.style-scope.px-vis-event').textContent.replace(/\s\s+/g, ''),'Event: DefaultID: 333Timestamp: 20:13:00 +0000 | 10 Apr 2014');
-    });
-  }); //suite
-
-  suite('px-vis-event faEvent works', function() {
-    var baseScale = document.getElementById('baseScale'),
-        baseSVG = document.getElementById('baseSVG'),
-        faEvent = document.getElementById('faEvent');
-
-    var colors = PxColorsBehavior.baseColors.properties.colors.value;
-
-    test('faEvent eventGroup created', function() {
-      assert.equal(faEvent.eventGroup.node().tagName,'g');
-    });
-    test('faEvent eventGroup has class', function() {
-      assert.equal(faEvent.eventGroup.attr('class'),'event');
-    });
-
-    test('faEvent eventIcon created', function() {
-      assert.equal(faEvent.eventIcon.node().tagName,'text');
-    });
-    test('faEvent eventIcon font-family', function() {
-      assert.equal(faEvent.eventIcon.attr('font-family'),'FontAwesome');
-    });
-    test('faEvent eventIcon font-size', function() {
-      assert.equal(faEvent.eventIcon.attr('font-size'),'16px');
-    });
-    test('faEvent eventIcon fill color', function() {
-      assert.equal(faEvent.eventIcon.attr('fill').split(' ').join(''),colors['blue']);
-    });
-    test('faEvent eventIcon icon', function() {
-      // This is weird. the  is the icon, but doesnt render in the text editor since it doesnt have the font file. so pulling the unicode from http://fontawesome.io/cheatsheet/ to test.
-      assert.equal(faEvent.eventIcon.text(),'\uf030');
-    });
-    test('faEvent eventIcon x', function() {
-      assert.equal(faEvent.eventIcon.attr('x'),352);
-    });
-    test('faEvent eventIcon y', function() {
-      assert.equal(faEvent.eventIcon.attr('y'),-5);
-    });
-
-    test('faEvent eventLine created', function() {
-      assert.equal(faEvent.eventLine.node().tagName,'line');
-    });
-    test('faEvent eventLine stroke color', function() {
-      assert.equal(faEvent.eventLine.attr('stroke').split(' ').join(''),colors['grey9']);
-    });
-    test('faEvent eventLine stroke width', function() {
-      assert.equal(faEvent.eventLine.attr('stroke-width'),1);
-    });
-    test('faEvent eventLine x1', function() {
-      assert.equal(faEvent.eventLine.attr('x1'),360);
-    });
-    test('faEvent eventLine x2', function() {
-      assert.equal(faEvent.eventLine.attr('x2'),360);
-    });
-    test('faEvent eventLine y1', function() {
-      assert.equal(faEvent.eventLine.attr('y1'),275);
-    });
-    test('faEvent eventLine y2', function() {
-      assert.equal(faEvent.eventLine.attr('y2'),0);
-    });
-
-    test('tooltip exists', function() {
-      assert.isTrue(faEvent.$$('px-tooltip') !== null);
-    });
-    test('tooltip content is correct', function() {
-      assert.equal(faEvent.$$('px-tooltip').$.tooltip.querySelector('span.style-scope.px-vis-event').textContent.replace(/\s\s+/g, ''),'Event: font awesomeID: 789Timestamp: 04:19:00 +0000 | 11 Apr 2014');
-    });
-  }); //suite
-
-  suite('px-vis-event uniEvent works', function() {
-    var baseScale = document.getElementById('baseScale'),
-        baseSVG = document.getElementById('baseSVG'),
-        uniEvent = document.getElementById('uniEvent');
-
-    var colors = PxColorsBehavior.baseColors.properties.colors.value;
-
-    test('uniEvent eventGroup created', function() {
-      assert.equal(uniEvent.eventGroup.node().tagName,'g');
-    });
-    test('uniEvent eventGroup has class', function() {
-      assert.equal(uniEvent.eventGroup.attr('class'),'event');
-    });
-
-    test('uniEvent eventIcon created', function() {
-      assert.equal(uniEvent.eventIcon.node().tagName,'text');
-    });
-    test('uniEvent eventIcon font-family', function() {
-      assert.equal(uniEvent.eventIcon.attr('font-family'),'FontAwesome');
-    });
-    test('uniEvent eventIcon font-size', function() {
-      assert.equal(uniEvent.eventIcon.attr('font-size'),'16px');
-    });
-    test('uniEvent eventIcon fill color', function() {
-      assert.equal(uniEvent.eventIcon.attr('fill').split(' ').join(''),colors['green']);
-    });
-    test('uniEvent eventIcon icon', function() {
-      // This is weird. the  is the icon, but doesnt render in the text editor since it doesnt have the font file. so pulling the unicode from http://fontawesome.io/cheatsheet/ to test.
-      assert.equal(uniEvent.eventIcon.text(),'\uf015');
-    });
-    test('uniEvent eventIcon x', function() {
-      assert.equal(uniEvent.eventIcon.attr('x'),-7);
-    });
-    test('uniEvent eventIcon y', function() {
-      assert.equal(uniEvent.eventIcon.attr('y'),-5);
-    });
-
-    test('uniEvent eventLine created', function() {
-      assert.equal(uniEvent.eventLine.node().tagName,'line');
-    });
-    test('uniEvent eventLine stroke color', function() {
-      assert.equal(uniEvent.eventLine.attr('stroke').split(' ').join(''),colors['grey9']);
-    });
-    test('uniEvent eventLine stroke width', function() {
-      assert.equal(uniEvent.eventLine.attr('stroke-width'),1);
-    });
-    test('uniEvent eventLine x1', function() {
-      assert.equal(uniEvent.eventLine.attr('x1'),0);
-    });
-    test('uniEvent eventLine x2', function() {
-      assert.equal(uniEvent.eventLine.attr('x2'),0);
-    });
-    test('uniEvent eventLine y1', function() {
-      assert.equal(uniEvent.eventLine.attr('y1'),275);
-    });
-    test('uniEvent eventLine y2', function() {
-      assert.equal(uniEvent.eventLine.attr('y2'),0);
-    });
-
-    test('tooltip exists', function() {
-      assert.isTrue(uniEvent.$$('px-tooltip') !== null);
-    });
-    test('tooltip content is correct', function() {
-      assert.equal(uniEvent.$$('px-tooltip').$.tooltip.querySelector('span.style-scope.px-vis-event').textContent.replace(/\s\s+/g, ''),'Event: unicodeID: 456Timestamp: 04:01:00 +0000 | 10 Apr 2014');
     });
   }); //suite
 
@@ -482,18 +351,15 @@ function runTests(){
     test('imgEvent eventIcon height', function() {
       assert.equal(imgEvent.eventIcon.attr('height'),'20px');
     });
-    test('imgEvent eventIcon x', function() {
-      assert.equal(imgEvent.eventIcon.attr('x'),110);
-    });
-    test('imgEvent eventIcon y', function() {
-      assert.equal(imgEvent.eventIcon.attr('y'),-25);
+    test('imgEvent eventIcon transform', function() {
+      assert.equal(imgEvent.eventIcon.attr('transform'),'translate(110,-25)');
     });
 
     test('imgEvent eventLine created', function() {
       assert.equal(imgEvent.eventLine.node().tagName,'line');
     });
     test('imgEvent eventLine stroke color', function() {
-      assert.equal(imgEvent.eventLine.attr('stroke').split(' ').join(''),colors['grey9']);
+      assert.equal(imgEvent.eventLine.attr('stroke').split(' ').join(''),rgbToHex(colors['grey8']));
     });
     test('imgEvent eventLine stroke width', function() {
       assert.equal(imgEvent.eventLine.attr('stroke-width'),1);
@@ -533,34 +399,15 @@ function runTests(){
       assert.equal(noLabelEvent.eventGroup.attr('class'),'event');
     });
 
-    test('noLabelEvent eventIcon created', function() {
-      assert.equal(noLabelEvent.eventIcon.node().tagName,'text');
-    });
-    test('noLabelEvent eventIcon font-family', function() {
-      assert.equal(noLabelEvent.eventIcon.attr('font-family'),'FontAwesome');
-    });
-    test('noLabelEvent eventIcon font-size', function() {
-      assert.equal(noLabelEvent.eventIcon.attr('font-size'),'16px');
-    });
-    test('noLabelEvent eventIcon fill color', function() {
-      assert.equal(noLabelEvent.eventIcon.attr('fill').split(' ').join(''),colors['grey7']);
-    });
-    test('noLabelEvent eventIcon icon', function() {
-      // This is weird. the  is the icon, but doesnt render in the text editor since it doesnt have the font file. so pulling the unicode from http://fontawesome.io/cheatsheet/ to test.
-      assert.equal(noLabelEvent.eventIcon.text(),'\uf05a');
-    });
-    test('noLabelEvent eventIcon x', function() {
-      assert.equal(noLabelEvent.eventIcon.attr('x'),29);
-    });
-    test('noLabelEvent eventIcon y', function() {
-      assert.equal(noLabelEvent.eventIcon.attr('y'),-5);
+    test('noLabelEvent eventIcon transform', function() {
+      assert.equal(noLabelEvent.eventIcon.attr('transform'), 'translate(29,-21) scale(1)');
     });
 
     test('noLabelEvent eventLine created', function() {
       assert.equal(noLabelEvent.eventLine.node().tagName,'line');
     });
     test('noLabelEvent eventLine stroke color', function() {
-      assert.equal(noLabelEvent.eventLine.attr('stroke').split(' ').join(''),colors['grey9']);
+      assert.equal(noLabelEvent.eventLine.attr('stroke').split(' ').join(''),rgbToHex(colors['grey8']));
     });
     test('noLabelEvent eventLine stroke width', function() {
       assert.equal(noLabelEvent.eventLine.attr('stroke-width'),1);
@@ -601,33 +448,18 @@ function runTests(){
     });
 
     test('offsetEvent eventIcon created', function() {
-      assert.equal(offsetEvent.eventIcon.node().tagName,'text');
+      assert.equal(offsetEvent.eventIcon.node().tagName,'g');
     });
-    test('offsetEvent eventIcon font-family', function() {
-      assert.equal(offsetEvent.eventIcon.attr('font-family'),'FontAwesome');
-    });
-    test('offsetEvent eventIcon font-size', function() {
-      assert.equal(offsetEvent.eventIcon.attr('font-size'),'16px');
-    });
-    test('offsetEvent eventIcon fill color', function() {
-      assert.equal(offsetEvent.eventIcon.attr('fill').split(' ').join(''),colors['red']);
-    });
-    test('offsetEvent eventIcon icon', function() {
-      // This is weird. the  is the icon, but doesnt render in the text editor since it doesnt have the font file. so pulling the unicode from http://fontawesome.io/cheatsheet/ to test.
-      assert.equal(offsetEvent.eventIcon.text(),'\uf069');
-    });
-    test('offsetEvent eventIcon x', function() {
-      assert.equal(offsetEvent.eventIcon.attr('x'),280);
-    });
-    test('offsetEvent eventIcon y', function() {
-      assert.equal(offsetEvent.eventIcon.attr('y'),15);
+
+    test('offsetEvent eventIcon transform translate', function() {
+      assert.equal(offsetEvent.eventIcon.attr('transform').split(' ')[0], 'translate(280,-1)');
     });
 
     test('offsetEvent eventLine created', function() {
       assert.equal(offsetEvent.eventLine.node().tagName,'line');
     });
     test('offsetEvent eventLine stroke color', function() {
-      assert.equal(offsetEvent.eventLine.attr('stroke').split(' ').join(''),colors['grey9']);
+      assert.equal(offsetEvent.eventLine.attr('stroke').split(' ').join(''),rgbToHex(colors['grey8']));
     });
     test('offsetEvent eventLine stroke width', function() {
       assert.equal(offsetEvent.eventLine.attr('stroke-width'),1);
@@ -669,14 +501,14 @@ function runTests(){
     });
 
     test('linearEvent eventIcon created', function() {
-      assert.equal(linearEvent.eventIcon.node().tagName,'text');
+      assert.equal(linearEvent.eventIcon.node().tagName,'g');
     });
 
     test('linearEvent eventLine created', function() {
       assert.equal(linearEvent.eventLine.node().tagName,'line');
     });
     test('linearEvent eventLine stroke color', function() {
-      assert.equal(linearEvent.eventLine.attr('stroke').split(' ').join(''),colors['grey9']);
+      assert.equal(linearEvent.eventLine.attr('stroke').split(' ').join(''),rgbToHex(colors['grey8']));
     });
 
     test('linearEvent eventLine x1', function() {
@@ -715,14 +547,14 @@ function runTests(){
     });
 
     test('ordinalEvent eventIcon created', function() {
-      assert.equal(ordinalEvent.eventIcon.node().tagName,'text');
+      assert.equal(ordinalEvent.eventIcon.node().tagName,'g');
     });
 
     test('ordinalEvent eventLine created', function() {
       assert.equal(ordinalEvent.eventLine.node().tagName,'line');
     });
     test('ordinalEvent eventLine stroke color', function() {
-      assert.equal(ordinalEvent.eventLine.attr('stroke').split(' ').join(''),colors['grey9']);
+      assert.equal(ordinalEvent.eventLine.attr('stroke').split(' ').join(''),rgbToHex(colors['grey8']));
     });
 
     test('ordinalEvent eventLine x1', function() {
