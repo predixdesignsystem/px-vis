b9aeda773a87d8dc214389de00c2479d502a74ab
menkhaus-ge <katherine.menkhaus@ge.com>
2017-08-10 17:12:19 -0700

    added links to catalog

--- gulpfile.js
+++ gulpfile.js
@@ -69,7 +69,7 @@ gulp.task('serve', function() {
     server: ['./', 'bower_components'],
   });
 
-  gulp.watch(['css/*-styles.html', 'css/*-demo.css', '*.html', '*.js']).on('change', browserSync.reload);
+  gulp.watch(['css/*-styles.html', 'css/*-demo.css', '*.html', 'demo/*.html','*.js']).on('change', browserSync.reload);
   gulp.watch(['sass/*.scss'], ['sass']);
 });
 
