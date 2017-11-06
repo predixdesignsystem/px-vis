045c450ce4a31ffa805512c0898b11bb0d9be253
Benoit Chevalier <benoit.chevalier@ge.com>
2017-11-06 14:51:57 -0800

    clean time in worker

--- px-vis-worker.js
+++ px-vis-worker.js
@@ -41,11 +41,9 @@ var dataMapping = {},
     quadtrees = {}
     quadtreeBuilt = false;
 
-function reply(data, time) {
+function reply(data) {
 
-  var time2 = null;
-  postMessage({'data': data, 'timeIn': time, 'timeOut': time2});
-
+  postMessage({'data': data});
 }
 
 /**
@@ -53,17 +51,17 @@ function reply(data, time) {
  *
  * @method updateData
  */
-function updateData(eventData, time) {
+function updateData(eventData) {
   dataMapping[eventData.chartId] = eventData.data.chartData;
-  reply(null, time);
+  reply(null);
 }
 
 
-function deleteData(eventData, time) {
+function deleteData(eventData) {
   delete dataMapping[eventData.chartId];
   delete quadtrees[eventData.chartId];
 
-  reply(null, time);
+  reply(null);
 }
 
 /**
@@ -733,7 +731,7 @@ function returnClosestsQuadtreePoints(eventData, time) {
   }
 
   delete dataObj.timeStampsTracker;
-  reply(dataObj, time);
+  reply(dataObj);
 }
 
 /**
@@ -741,7 +739,7 @@ function returnClosestsQuadtreePoints(eventData, time) {
  *
  * @method returnQuadtreePointsInArea
  */
-function returnQuadtreePointsInArea(eventData, time) {
+function returnQuadtreePointsInArea(eventData) {
   var visData = eventData.data,
       quadtreeData = quadtrees[eventData.chartId],
       dataObj = createDataStub();
@@ -750,7 +748,7 @@ function returnQuadtreePointsInArea(eventData, time) {
     dataObj = searchAreaBoxQuadtree(quadtreeData, visData, dataObj);
   }
 
-  reply(dataObj, time);
+  reply(dataObj);
 }
 
 function emptySeries(k) {
@@ -776,7 +774,7 @@ function addCrosshairData(dataObj, d) {
   return dataObj;
 }
 
-function determineExtents(eventData, time) {
+function determineExtents(eventData) {
   var visData = eventData.data,
       extents = null;
 
@@ -795,13 +793,11 @@ function determineExtents(eventData, time) {
     extents = extentCalc.determineExtents(dataMapping[eventData.chartId]);
   }
 
-  reply(extents, time);
+  reply(extents);
 }
 
 onmessage = function(e) {
 
-  //var time = this.performance.now();
- var time = null;
   switch(e.data.action) {
 
     case 'init':
@@ -811,7 +807,7 @@ onmessage = function(e) {
         importScripts("../pxd3/d3.min.js");
       }
 
-      reply(null, time);
+      reply(null);
       break;
 
     case 'registerCustomScript':
@@ -820,7 +816,7 @@ onmessage = function(e) {
         importScripts(e.data.data.url);
       }
 
-      reply(null, time);
+      reply(null);
       break;
 
     case 'runCustomFunction':
@@ -841,11 +837,11 @@ onmessage = function(e) {
         throw 'Couldn\'t run custom function ' + e.data.data.functionName + ' on custom Object ' + e.data.data.objectName + ' because the specified Object doesn\'t exist. Make sure you have loaded your custom script defining the custom object.';
       }
 
-      reply(result, time);
+      reply(result);
       break;
 
     case 'updateData':
-      updateData(e.data, time);
+      updateData(e.data);
       break;
 
     case 'createQuadtree':
@@ -878,14 +874,14 @@ onmessage = function(e) {
       break;
 
     case 'determineExtents':
-      determineExtents(e.data, time);
+      determineExtents(e.data);
       break;
 
     case 'unregisterChart':
-      deleteData(e.data, time);
+      deleteData(e.data);
       break;
 
     default:
-      reply(null, time);
+      reply(null);
   }
 }
