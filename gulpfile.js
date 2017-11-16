073c094bd726ab69c3be8cf5a37970d97bba8aa4
Elliot Nahman <elliot.nahman@ge.com>
2017-11-16 08:10:51 -0800

    added blobfish gulp taks to create px-vis-scheduler file and use worker files as a blob

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,10 @@ const gulpif = require('gulp-if');
 const combiner = require('stream-combiner2');
 const bump = require('gulp-bump');
 const argv = require('yargs').argv;
+const fs = require("fs");
+const uglifyJS = require("uglify-js");
+const rename = require("gulp-rename");
+const through = require('through2');
 
 const sassOptions = {
   importer: importOnce,
@@ -57,6 +61,7 @@ gulp.task('sass', function() {
 
 gulp.task('watch', function() {
   gulp.watch(['sass/*.scss'], ['sass']);
+  gulp.watch(['./px-vis-worker.js', './px-vis-worker-scale.js', 'scheduler-dev.html'], ['blobfish']);
 });
 
 gulp.task('serve', function() {
@@ -93,4 +98,48 @@ gulp.task('bump:major', function(){
 
 gulp.task('default', function(callback) {
   gulpSequence('clean', 'sass')(callback);
+});
+
+/*
+Takes our worker files and concatenates them into a single variable to be inserted into our scheduler. That way, we can just use a blob to create our workers instead of requiring outside files.
+*/
+gulp.task('blobfish', function() {
+  // files to blobify
+  const scale = fs.readFileSync("px-vis-worker-scale.js", "utf8");
+  const worker = fs.readFileSync("px-vis-worker.js", "utf8");
+  const d3 = fs.readFileSync("bower_components/pxd3/d3.min.js", "utf8");
+
+  // choose if we should minify said files (d3 is already minified)
+  const fugly = argv.dev ? (scale + worker) : uglifyJS.minify({"f1": scale, "f2": worker}).code;
+
+  // concatenate blob files into one
+  const concatenated = d3 + fugly;
+
+  // store as json to force the output to be a string when inserted into the scheduler.
+  const json = { script: concatenated };
+  const str = JSON.stringify(json);
+
+  // our custom gulp "package" to run our regex on the stream. :vsign: stupid gulp regex packages
+  function replace(newStr) {
+    return through.obj(function(file, enc, callback) {
+      if(('contents' in file) && !file.isNull() && !file.isStream() &&file.isBuffer()) {
+        const contents = String(file.contents);
+        const re = /blobyblobblobblob/;
+        const matches = contents.replace(re, newStr);
+
+        file.contents = new Buffer(matches);
+        this.push(file);
+        return callback();
+
+      } else {
+        throw "something went wrong while regexing"
+      }
+    });
+  }
+
+  return gulp.src('./scheduler-dev.html')
+    .pipe(replace(str))
+    .pipe(rename('px-vis-scheduler.html'))
+    .pipe(gulp.dest('./'));
+
 });
